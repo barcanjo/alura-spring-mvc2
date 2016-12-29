a416c2230e0960a7172ab8c7cd53f3434b86e3a3
Bruno Arcanjo <bm.arcanjo@gmail.com>
2016-12-28 22:08:06 -0200

    mudando o locale dinamicamente

--- casadocodigo/src/main/java/org/casadocodigo/loja/conf/AppWebConfiguration.java
+++ casadocodigo/src/main/java/org/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -22,10 +22,14 @@ import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -66,7 +70,7 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 	@Bean
 	public MessageSource messageSource() {
 		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-		messageSource.setBasename("/WEB-INF/messages");
+		messageSource.setBasename("/WEB-INF/message");
 		messageSource.setDefaultEncoding("UTF-8");
 		messageSource.setCacheSeconds(1);
 		
@@ -167,5 +171,21 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 	    resolver.setContentNegotiationManager(manager);
 	    
 	    return resolver;
+	}
+	
+	/**
+	 * Adiciona um novo interceptador que verificará a mudança de lcale do usuário
+	 */
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LocaleChangeInterceptor());
+	}
+	
+	/**
+	 * Cria um bean que armazena a configuração do locale do usuário e carrega as páginas no idioma correto.
+	 * @return Uma instância de bean da interface LocalResolver com as configurações para resolver locales.
+	 */
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new CookieLocaleResolver();
 	}
 }
